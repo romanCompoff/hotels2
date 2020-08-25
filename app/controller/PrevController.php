70f2a465e5154fb52210316d08240c52834b08d6
Vitaliy Dorokhin <s1te@bk.ru>
2020-08-26 02:46:11 +0300

    get add hotels

--- app/controller/PrevController.php
+++ app/controller/PrevController.php
@@ -1,14 +1,15 @@
 <?php
 namespace Dva\Hotels\Controller;
 
-use Dva\Hotels\Model\GAdsModel;
+use Dva\Hotels\Model\PrevModel;
 use Dva\Hotels\core\DB;
 
 class PrevController Extends AdminController
 {
-	public function firstFunc()
+	public function addPrev($enName, $post)
 	{
-		
+		$mPost = new PrevModel(DB::getConnect());
+		$mPost->addPrev($enName, $post);
 	}
 	
 			public function outputForm()
@@ -17,24 +18,5 @@ class PrevController Extends AdminController
 			$this->content = $this->build($this->myPath('prevView/addPrevView'), []);
 		}
 		
-			public static function translit_sef($value)
-		{
-			$converter = array(
-				'а' => 'a',    'б' => 'b',    'в' => 'v',    'г' => 'g',    'д' => 'd',
-				'е' => 'e',    'ё' => 'e',    'ж' => 'zh',   'з' => 'z',    'и' => 'i',
-				'й' => 'y',    'к' => 'k',    'л' => 'l',    'м' => 'm',    'н' => 'n',
-				'о' => 'o',    'п' => 'p',    'р' => 'r',    'с' => 's',    'т' => 't',
-				'у' => 'u',    'ф' => 'f',    'х' => 'h',    'ц' => 'c',    'ч' => 'ch',
-				'ш' => 'sh',   'щ' => 'sch',  'ь' => '',     'ы' => 'y',    'ъ' => '',
-				'э' => 'e',    'ю' => 'yu',   'я' => 'ya',
-			);
 		
-			$value = mb_strtolower($value);
-			$value = strtr($value, $converter);
-			$value = mb_ereg_replace('[^-0-9a-z]', '-', $value);
-			$value = mb_ereg_replace('[-]+', '-', $value);
-			$value = trim($value, '-');	
-		
-			return $value;
-		}
 }
